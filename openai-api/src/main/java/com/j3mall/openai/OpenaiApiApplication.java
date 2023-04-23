6238e5285d7f8fa7fc9f578e4457545de439c58c
Teddy Jiang <teddy.jiang@qq.com>
2023-04-23 18:50:09 +0800

    初始化项目

---
+++ openai-api/src/main/java/com/j3mall/openai/OpenaiApiApplication.java
@@ -0,0 +1,13 @@
+package com.j3mall.openai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenaiApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenaiApiApplication.class, args);
+    }
+
+}
